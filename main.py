c6ca4755b2e75b8cd1f768a19ada46fd78b3a51c
akhilrajs <67222042+akhilrajs@users.noreply.github.com>
2021-06-09 17:33:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 while True:
-    from tabulate import tabulate
-    from time import sleep
-    from os import system
-    
     def main():
         print("# PDF tools")
         print("# author : akhilrajs")
-        print(tabulate([['Option', 'Tool'],['------','----'],['1','Merge PDFs in a given folder'],['2','Split a PDF into individual pages']]))
+        from tabulate import tabulate
+        print(tabulate([['Option', 'Tool'],['------','----'],['1','Merge PDFs in a given folder'],['2','Split a PDF into individual pages'],
+                        ['3','Extract text from PDF']
+                        ,['4','Convert PDF to JPEG']]))
         print()
         tool_selected = input("# Enter option required :> ")
         if  tool_selected =="1":
@@ -17,15 +16,33 @@ while True:
         elif tool_selected == "2":
             try:
                 print()
-                print("# Optoion (2) selected : Split a PDF into individual pages")
+                print("# Option (2) selected : Split a PDF into individual pages")
                 from split import split_main
                 split_main()
+            except Exception as e:
+                print("# ERROR : " + str(e))
+        elif tool_selected == "3":
+            try:
+                print()
+                print("# Option (3) selected : Extract text from PDF")
+                from extract_text import extract_text_main
+                extract_text_main()
+            except Exception as e:
+                print("# ERROR : " + str(e))
+        elif tool_selected == "4":
+            try:
+                print()
+                print("# Option (4) selected : Convert PDF to JPEG")
+                from pdftojpeg import pdf_to_jpeg_main
+                pdf_to_jpeg_main()
             except Exception as e:
                 print("# ERROR : " + str(e))
         else:
             print("# invalid option entered")
             print('# try again ')
+            from time import sleep
             sleep(1.2)
+            from os import system
             system("cls")
             main()
     main()
---
+++ pdftojpeg.py
@@ -0,0 +1,29 @@
+def pdf_to_jpeg_main():
+    from time import sleep
+    from os import path
+    from os import makedirs
+    from os import getcwd
+    from os import system 
+    from os import startfile
+    from shutil import move
+    from pdf2image import convert_from_path
+    
+    folder = 'JPEGS_of_PDF/'
+    directory = (getcwd()) + "/OUTPUT/"
+    SAVE_PATH = path.join(directory, folder)
+    if not path.exists(SAVE_PATH):
+        makedirs(SAVE_PATH)
+    system("cls")
+    print("# PDF TO JPEG TOOL")
+    print('# author - akhilrajs')
+    print('---------------------')
+    sleep(1)
+    print()
+    PDF_location = input('''# Drag and drop the PDF here :> ''')
+    images = convert_from_path(PDF_location,500, poppler_path = r'poppler-0.68.0\bin')
+    for i in range(len(images)):
+        images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+        source = "page" + str(i) + '.jpg'
+        destination = SAVE_PATH
+        move(source, destination)
+    system("cls")        
